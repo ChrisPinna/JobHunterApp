65ef35da2120aef5e0f5c5ed85b44b22d3bbb827
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-19 15:52:15 +0100

    Delete jobhunt frontend logic
    Co-authored-by: Christian Pinna <ChrisPinna@users.noreply.github.com>

--- client/src/components/JobHuntModal.tsx
+++ client/src/components/JobHuntModal.tsx
@@ -2,11 +2,13 @@ import { SetStateAction, useState } from "react";
 import { Modal, Button, Group, useMantineTheme } from "@mantine/core";
 import styled from "styled-components";
 import { JobHuntType } from "../typings";
+import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
 
 const Container = styled.div``;
 const Wrapper = styled.div`
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 const Title = styled.span`
   margin-bottom: 0.5rem;
@@ -23,23 +25,47 @@ const Status = styled.span`
   margin-top: 1rem;
 `;
 
+const DeleteIconContainer = styled.div`
+  position: absolute;
+  top: 0rem;
+  right: 0rem;
+`;
+
 type Props = {
   modalOpened: boolean;
   setModalOpened: React.Dispatch<React.SetStateAction<boolean>>;
-	jobHunt: JobHuntType
+  jobHunt: JobHuntType;
 };
-
 
 const JobHuntModal = ({ modalOpened, setModalOpened, jobHunt }: Props) => {
   const theme = useMantineTheme();
+
+  const handleDelete = async (e: any) => {
+    e.preventDefault();
+    try {
+      await axios.delete(`/jobhunts/${jobHunt.id}`);
+      setModalOpened(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Modal size="lg" opened={modalOpened} onClose={() => setModalOpened(false)} withCloseButton={false}>
+    <Modal
+      size="lg"
+      opened={modalOpened}
+      onClose={() => setModalOpened(false)}
+      withCloseButton={false}
+    >
       <Container>
         <Wrapper>
           <Title>{jobHunt.title}</Title>
           <ApplicationLink>{jobHunt.applicationLink}</ApplicationLink>
-					<Description>{jobHunt.description}</Description>
+          <Description>{jobHunt.description}</Description>
           <Status>{jobHunt.status}</Status>
+          <DeleteIconContainer>
+            <DeleteIcon onClick={handleDelete} />
+          </DeleteIconContainer>
         </Wrapper>
       </Container>
     </Modal>

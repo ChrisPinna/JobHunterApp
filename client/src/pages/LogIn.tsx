8c6c8e78100b81bebb08e8fea66e300636200d36
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-20 14:59:42 +0100

    Design log in page
    Co-authored-by: Christian Pinna <ChrisPinna@users.noreply.github.com>

--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
   margin-top: 10rem;
-`
+`;
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`
+`;
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`
+`;
 const Header = styled.h1`
   color: #fe7a1b;
   padding: 3rem;
-`
+`;
 const InputContainer = styled.div`
   display: flex;
   gap: 0.5rem;
-`
+`;
 const Input = styled.input`
   padding: 0.5rem;
   border-radius: 0.5rem;
@@ -38,9 +38,9 @@ const Input = styled.input`
       red 0 -18px 40px, -12px 14px 20px -19px rgba(0, 0, 0, 0);
     box-shadow: #fff 0 -1px 4px, #ff0 0 -2px 10px, #ff8000 0 -10px 20px,
       red 0 -18px 40px, -12px 14px 20px -19px rgba(0, 0, 0, 0);
-      cursor: pointer;
+    cursor: pointer;
   }
-`
+`;
 const BottomContainer = styled.div`
   display: flex;
   align-items: center;
@@ -49,7 +49,7 @@ const BottomContainer = styled.div`
   &:hover {
     cursor: pointer;
   }
-`
+`;
 const SubmitButton = styled.button`
   border-radius: 0.5rem;
   border: none;
@@ -65,36 +65,66 @@ const SubmitButton = styled.button`
       red 0 -18px 40px, -12px 14px 20px -19px rgba(0, 0, 0, 0);
     cursor: pointer;
   }
-`
+`;
 const LoginPrompt = styled.span`
   font-size: 12px;
   color: blue;
   text-decoration: underline;
-`
+`;
 
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <Container>
-        <Wrapper>
-            <Header>Welcome to Job Hunter</Header>
+      <Wrapper>
+        {isLogin ? (
+          <>
+            <Header>Log In!</Header>
             <Form>
-                <InputContainer>
-                  <Input placeholder='First Name' />
-                  <Input placeholder='Last Name'/>
-                </InputContainer>
-                <Input placeholder='Email' type="email" style={{width: "100%"}}/>
-                <InputContainer>
-                  <Input placeholder='Password' type="password"/>
-                  <Input placeholder='Confirm Password' type="password"/>
-                </InputContainer>
-                <BottomContainer>
-                <LoginPrompt>Already have an account? Click here to log in</LoginPrompt>
+              <Input
+                placeholder="Email"
+                type="email"
+                style={{width: "100%"}}
+              />
+              <Input placeholder="Password" type="password" style={{width: "100%"}}/>
+              <BottomContainer>
+                <LoginPrompt onClick={()=>setIsLogin(false)}>
+                  Don't have an account? Sign up here!
+                </LoginPrompt>
                 <SubmitButton>Sign Up</SubmitButton>
-                </BottomContainer>
+              </BottomContainer>
             </Form>
-        </Wrapper>
+          </>
+        ) : (
+          <>
+            <Header>Sign Up to Job Hunter</Header>
+            <Form>
+              <InputContainer>
+                <Input placeholder="First Name" />
+                <Input placeholder="Last Name" />
+              </InputContainer>
+              <Input
+                placeholder="Email"
+                type="email"
+                style={{ width: "100%" }}
+              />
+              <InputContainer>
+                <Input placeholder="Password" type="password" />
+                <Input placeholder="Confirm Password" type="password" />
+              </InputContainer>
+              <BottomContainer>
+                <LoginPrompt onClick={()=>setIsLogin(true)}>
+                  Already have an account? Click here to log in
+                </LoginPrompt>
+                <SubmitButton>Sign Up</SubmitButton>
+              </BottomContainer>
+            </Form>
+          </>
+        )}
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
